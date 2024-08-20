987fd07c612c8694b71af58f93bc295fb121a72c
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-08-20 10:37:14 +0200

    old changes

--- server/app.py
+++ server/app.py
@@ -1,42 +1,68 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask, CORS
-from flask_cors import CORS
+from flask import Flask, request, jsonify
+# from flask_cors import CORS
 
 CREATE_USERS_TABLE = (
-    """CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        username VARCHAR);"""
+    """CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username VARCHAR);"""
 )
 
-INSERT_USER_RETURN_ID = (
-    """INSERT INTO users (username) VALUES ('pablo');"""
+INSERT_USER = (
+    """INSERT INTO users (username) VALUES (%s);"""
 )
 
 load_dotenv()
 
 # Creates the app instance
 app = Flask(__name__)
+
+# Access the .env file to obtain the DB URL
 url = os.getenv("DATABASE_URL")
+
+# Connect to Postgres DB with psycopg2 and pass URL
 connection = psycopg2.connect(url)
 
-cors = CORS(app, origins='*')
+# Allow Cross-Origin
+# cors = CORS(app, origins='*')
 
-@app.route("/", methods=['GET'])
+@app.get("/")
 def home():
-    return "Hello world"
+    return "<h1>Hello world</h1>"
 
-@app.route("/api/user", methods=['POST'])
+""" @app.post("/api/user")
 def create_user():
     data = request.get_json()
     username = data["username"]
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(CREATE_USERS_TABLE)
-            cursor.execute(INSERT_USER_RETURN_ID (username,))
-            user_id = cursor.fetchone()[0]
-    return {"id": user_id, "message": f"User created with username: {username}"}, 201
+            cursor.execute(INSERT_USER, (username))
+    return {"message": f"user created with username: {username}"}, 201 """
+
+@app.route("/api/user", methods=['POST'])
+def post_user():
+    if request.is_json:
+        data = request.get_json()
+        username = data.username
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(INSERT_USER, (username,))
+        return jsonify(data), 200
+    else:
+        return jsonify({"error": "Invalid content type"}), 400
+
+@app.route('/post_json', methods=['POST'])
+def post_json():
+    # Check if the request content type is JSON
+    if request.is_json:
+        data = request.get_json()  # Parse the JSON data
+        response = {
+            "message": "JSON received!",
+            "data": data
+        }
+        return jsonify(response), 200
+    else:
+        return jsonify({"error": "Invalid content type"}), 400
     
     
 if __name__ == "__main__":
